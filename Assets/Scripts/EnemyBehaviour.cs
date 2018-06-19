a4cc2718c496cfd7c8f3d0d2c40197d5d078b2fc
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-06-19 15:35:47 -0500

    added player movement and enemy behaviour

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float Speed = 20;
+
+    private GameObject Player;
+	// Use this for initialization
+	void Start ()
+	{
+	    Player = GameObject.Find("Player");
+	    Speed *= 500;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 direction = Player.transform.position - transform.position;
+        direction.Normalize();
+	    Rigidbody rb3d = GetComponent<Rigidbody>();
+        rb3d.AddForce(direction*Speed*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float Speed = 20;
+	// Use this for initialization
+	void Start ()
+	{
+	    Speed *= 500;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Rigidbody rb3d = GetComponent<Rigidbody>();
+	    if (Input.GetButton("Forward"))
+	        rb3d.AddForce(Vector3.forward * Speed * Time.deltaTime);
+	    if (Input.GetButton("Backward"))
+	        rb3d.AddForce(Vector3.back * Speed * Time.deltaTime);
+	    if (Input.GetButton("Left"))
+	        rb3d.AddForce(Vector3.left * Speed * Time.deltaTime);
+	    if (Input.GetButton("Right"))
+	        rb3d.AddForce(Vector3.right * Speed * Time.deltaTime);
+    }
+}
