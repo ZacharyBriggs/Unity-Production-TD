63fa7ce6a9781a35bf6a4a0630fb89971121fff3
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-06-20 18:49:56 -0500

    added path scriptable object

--- Assets/Scripts/PayloadBehaviour.cs
+++ Assets/Scripts/PayloadBehaviour.cs
@@ -7,21 +7,21 @@ public class PayloadBehaviour : MonoBehaviour
 {
     private int CurrentNode = 0;
     private int NextNode = 1;
-    public List<Transform> NavNodes;
+    public PathSriptable Path;
     private NavMeshAgent Agent;
     public float HP = 100;
     // Use this for initialization
     void Start()
     {
-        this.transform.position = NavNodes[CurrentNode].position;
+        this.transform.position = Path.Steps[CurrentNode];
         Agent = GetComponent<NavMeshAgent>();
-        Agent.destination = NavNodes[NextNode].position;
+        Agent.destination = Path.Steps[NextNode];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.transform.position.x == NavNodes[NextNode].position.x && this.transform.position.y == NavNodes[NextNode].position.z)
+        if(this.transform.position.x == Path.Steps[NextNode].x && this.transform.position.z == Path.Steps[NextNode].z)
             ChangeDestination();
         if (Input.GetKeyDown(KeyCode.Space))
             HP -= 10;
@@ -33,6 +33,6 @@ public class PayloadBehaviour : MonoBehaviour
     {
         CurrentNode = NextNode;
         NextNode += 1;
-        Agent.destination = NavNodes[NextNode].position;
+        Agent.destination = Path.Steps[NextNode];
     }
 }
