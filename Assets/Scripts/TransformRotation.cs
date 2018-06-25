61ea6e6e6cc02509d88ed67ef611af1c99f6c331
Ralenski Doucet <Ralenskid@gmail.com>
2018-06-25 17:58:20 -0500

    COMMIT

---
+++ Assets/Scripts/TransformRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private float smooth = 5.0f;
+
+    private float TiltAngle = 60.0f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+        float TiltAroundZ = Input.GetAxis("Horizontal") * TiltAngle;
+	    float TiltAroundX = Input.GetAxis("Vertical") * TiltAngle;
+        Quaternion target =Quaternion.Euler(TiltAroundX,0,TiltAroundZ);
+        transform.rotation=Quaternion.Slerp(transform.rotation,target,Time.deltaTime * smooth);
+
+    }
+}
