c3cac6bd315fa7c57d198cb5583622afe700d9e3
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-03 17:38:46 -0500

    Got Targeting system to log whats in front of gun

--- Assets/Scripts/CrossHairBehaviour.cs
+++ Assets/Scripts/CrossHairBehaviour.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class CrossHairBehaviour 
+public class CrossHairBehaviour : MonoBehaviour
 {
-    
-    public Vector2 crosshairnormposition;
-	// Use this for initialization
-	void Start ()
-	{
-
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    
-   
+    public GameObject gun;
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.transform.position = Input.mousePosition;
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var crossHairPos = gun.transform.position + (gun.transform.forward * 10);
+        Debug.DrawLine(gun.transform.position, crossHairPos);
+        transform.position = Camera.main.WorldToScreenPoint(crossHairPos);
+        Debug.Log(crossHairPos);
+    }
+
 }
