ebbfbf43855e41fb671f41660c1c0b27566b9d22
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-09 10:44:10 -0500

    added health scriptalble object

---
+++ Assets/Scripts/HealthBarBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarBehaviour : MonoBehaviour
+{
+
+    private Slider HealthBar;
+
+    void OnValueChange()
+    {
+        var health = 100;
+        if (health > 0)
+        {
+            
+        }
+
+        
+    }
+	// Use this for initialization
+	void Start ()
+	{
+	    var health = 100;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/HealthScriptable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Health")]
+public class HealthScriptable : ScriptableObject
+{
+    public int Health;
+
+    public virtual void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Debug.Log("Object is dead.");
+        }
+    }
+}
