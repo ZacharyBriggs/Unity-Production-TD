ca75f81933fbf2092325c420de2c271e05e4a7e2
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-09 16:46:01 -0500

    commit

--- Assets/Scripts/TransformRotationBehaviour.cs
+++ Assets/Scripts/TransformRotationBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformRotation : MonoBehaviour {
+public class TransformRotationBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
