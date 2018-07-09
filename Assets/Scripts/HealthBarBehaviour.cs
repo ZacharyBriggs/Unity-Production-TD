31eec1db70d343f364a2f244aaee8b8f928e14ae
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-09 11:42:40 -0500

    working on health slider

--- Assets/Scripts/HealthBarBehaviour.cs
+++ Assets/Scripts/HealthBarBehaviour.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBarBehaviour : MonoBehaviour
+public class HealthBarBehaviour :HealthScriptable  
 {
 
     private Slider HealthBar;
 
-    void OnValueChange()
+    void OnValueChanged(HealthScriptable healthBar)
     {
-        var health = 100;
-        if (health > 0)
+        if (HealthBar.value > 0)
         {
-            
+           HealthBar.value = -10;
         }
-
-        
     }
 	// Use this for initialization
 	void Start ()
 	{
-	    var health = 100;
+	    HealthBar.value = Health;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		TakeDamage(Health);
+        
 	}
 }
