b07b39be2c1573640f5fab9cd20244da43e99f28
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-10 15:18:38 -0500

    started enemy health bar display

--- Assets/Scripts/EnemyHealthBarBehaviour.cs
+++ Assets/Scripts/EnemyHealthBarBehaviour.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealthBarBehaviour : MonoBehaviour {
-
+public class EnemyHealthBarBehaviour : MonoBehaviour
+{
+    private EnemyBehaviour Enemy;
+    private SpriteRenderer HealthBarImage;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    Enemy = GetComponent<EnemyBehaviour>();
+	    HealthBarImage = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
 	}
 }
