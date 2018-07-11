9b3cb3a11508fb563e2045baa9f48eef1c31988a
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-11 13:03:23 -0500

    IntVariable
    scripts now use intvariable to update there ui elements

---
+++ Assets/Scripts/IntVariable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public abstract class IntVariable : ScriptableObject
+{
+    public abstract int Value { get; }
+    public abstract int MaxValue { get; }
+    public delegate void OnValueChanged();
+    public OnValueChanged onValueChanged;
+}
