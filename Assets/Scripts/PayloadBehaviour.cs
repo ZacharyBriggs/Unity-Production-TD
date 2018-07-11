e971a24fb8da12f7f97604440543ec294865e81f
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-11 13:04:10 -0500

    modify payloadbehaviour
    to accommodate future gameplay changes

---
+++ Assets/Scripts/PayloadBehaviour.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class PayloadBehaviour : MonoBehaviour
+{
+//ToDo: we need to have methods to change the values of the intvariable references    
+    private IntVariable Health;
+    private IntVariable Experience;
+
+
+
+
+
+    #region zac
+
+    //public PathSriptable Path;
+
+    //private int CurrentNode = 0;
+    //private int NextNode = 1;
+    //private NavMeshAgent Payload;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //this.transform.position = Path.Steps[0];
+        //  Payload = GetComponent<NavMeshAgent>();
+        //    Payload.destination = Path.Steps[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.Health = HealthScript.Health;
+        //Payload.speed = 0;
+        //if(Path == null)
+        //    return;
+        //if (this.transform.position.x == Path.Steps[NextNode].x && this.transform.position.z == Path.Steps[NextNode].z && CurrentNode + 2 < Path.Steps.Count)
+        //    ChangeDestination();
+        //else if (this.transform.position.x == Path.Steps[Path.Steps.Count - 1].x &&
+        //         this.transform.position.z == Path.Steps[Path.Steps.Count - 1].z)
+        //{
+        //    Win();
+        //}
+        //#if UNITY_EDITOR
+        //        if (Input.GetKeyDown(KeyCode.Space))
+        //        {
+        //            this.TakeDamage(99999999);
+        //        }
+        //#endif
+        //    }
+
+        //    void ChangeDestination()
+        //    {
+        //        CurrentNode = NextNode;
+        //        NextNode += 1;
+        //        //Payload.destination = Path.Steps[NextNode];
+        //    }
+
+        //    public void TakeDamage(int amount)
+        //    {
+        //        HealthScript.TakeDamage(amount);
+        //        if (HealthScript._IntVariable <= 0)
+        //            Destroy(this.gameObject);
+        //    }
+        //    void Win()
+        //    {
+        //        Debug.Log("You Win!!!!");
+        //        SceneManager.LoadScene("mainmenu");
+        //    }
+
+        //    void OnTriggerStay(Collider collision)
+        //    {
+        //        if (collision.gameObject.tag == "Player")
+        //            Payload.speed = 3;
+        //    } 
+        #endregion
+    }
+}
