f60511abadeb9c37ef9205ae1a2bc43612ac2cdb
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-11 11:24:13 -0500

    Player Shooting Behaviour
    can now shoot up and down

--- Assets/Scripts/PlayerShootingBehaviour.cs
+++ Assets/Scripts/PlayerShootingBehaviour.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class PlayerShootingBehaviour : MachineGunBehaviour
 {
-
-	void Start ()
-	{
-		
-	}
-	
+    public Transform camera;
 	void Update ()
 	{
 	    if (Input.GetButtonDown("Fire1"))
 	    {
 	        RaycastHit hit = new RaycastHit();
-            if (Physics.Raycast(this.transform.position, this.transform.forward*1000,out hit))
+            if (Physics.Raycast(this.transform.position, Camera.main.transform.forward*1000,out hit))
 	        {
                 Debug.Log("Hit " + hit.collider.gameObject.tag);
-	            Debug.DrawRay(this.transform.position, this.transform.forward*1000, Color.yellow,1);
+	            Debug.DrawRay(this.transform.position, Camera.main.transform.forward*1000, Color.yellow,1);
                 if (hit.collider.gameObject.tag == "Enemy")
 	            {
                     Target = hit.collider.gameObject.GetComponent<EnemyBehaviour>();
