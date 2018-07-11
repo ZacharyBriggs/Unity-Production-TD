8ed307aa543eed587afbec74e35e74e7ac4e9d7c
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-11 13:04:26 -0500

    healthscriptable
    intvariable implementation

--- Assets/Scripts/HealthScriptable.cs
+++ Assets/Scripts/HealthScriptable.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[CreateAssetMenu(menuName = "Health")]
-public class HealthScriptable : ScriptableObject
+[CreateAssetMenu(menuName = "_IntVariable")]
+public class HealthScriptable : IntVariable
 {
-    public int Health;
+    [SerializeField] private int _mMaxValue = 5;
+    [SerializeField] private int _mValue = 1;
 
+    public int Health
+    {
+        get { return _mValue; }
+        set
+        {
+            _mValue = value;
+            if (onValueChanged != null)
+                onValueChanged();
+        }
+    }
+
+    public override int Value
+    {
+        get { return Health; }
+    }
+
+    public override int MaxValue
+    {
+        get { return _mMaxValue; }
+    }
+
+    //ToDo: Move TakeDamage to a MonoBehaviour or interface implementation
     public virtual void TakeDamage(int amount)
     {
         Health -= amount;
-        if (Health <= 0)
-        {
-            Debug.Log("Object is dead.");
-        }
+        if (Health <= 0) Debug.Log("Object is dead.");
     }
 }
