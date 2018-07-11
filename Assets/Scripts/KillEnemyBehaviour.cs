bdaccf4a29f8f7c86829bca6318f178b8b35760f
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-11 13:04:57 -0500

    killenemy
    killenemy will only do the killing of enemies now

---
+++ Assets/Scripts/KillEnemyBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class KillEnemyBehaviour : MonoBehaviour
+{
+    private void KillEnemy()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (gameObject.name == "Enemy")
+                DestroyObject(gameObject);
+    }
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        KillEnemy();
+    }
+}
