aa01639dfa582e364aadcdec961be339db03ee68
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-16 18:10:30 -0500

    wavemanager for spawning and handling waves

---
+++ Assets/Scripts/WaveManagerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManagerBehaviour : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public WaveScriptable WaveInfo;
+
+    public bool Active;
+    public void SpawnWave()
+    {
+        if (Active)
+            return;
+        Active = true;
+        for (int i = 0; i < WaveInfo.MaxEnemies; i++)
+        {
+            var e = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+            e.GetComponent<EnemyBehaviour>().OnEnemyDied.AddListener(OnEnemyDied);
+        }
+
+    }
+    public int DeathCount = 0;
+    public void OnEnemyDied()
+    {
+        DeathCount++;
+        if (DeathCount >= WaveInfo.MaxEnemies)
+        {
+            OnWaveComplete.Raise();
+        }
+    }
+    public GameEvent OnWaveComplete;
+}
