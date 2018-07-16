9eb5c24590b405c0feab245c51ab1958db3085d2
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-16 18:07:52 -0500

    created step scriptable that takes in a position, wave scriptable, and bool

---
+++ Assets/Scripts/StepScriptable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Step")]
+public class StepScriptable : ScriptableObject
+{
+    public Vector3 position;
+    public bool IsWaveNode;
+    public WaveScriptable Wave;
+}
