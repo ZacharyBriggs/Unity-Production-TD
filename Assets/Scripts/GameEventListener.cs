8d8c7f3ed0703e00d5b059e449a7c8ed6a4f3484
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-16 17:50:50 -0500

    gameevent system

---
+++ Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// GameEventListener implementation
+/// ToDo: Usage docs
+/// </summary>
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent GameEvent;
+    public UnityEvent GameEventResponse;
+
+    private void OnEnable()
+    {
+        GameEvent.AddListener(this);
+    }
+
+    private void OnDisable()
+    {
+        GameEvent.RemoveListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        GameEventResponse.Invoke();
+    }
+}
---
+++ Assets/Scripts/PhysicsTriggerListener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsTriggerListener : MonoBehaviour
+{
+    [Cinemachine.TagField]
+    public string ListenerTag;
+    public GameEvent EnterTrigger;
+    public GameEvent ExitTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(ListenerTag))
+            EnterTrigger.Raise();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(ListenerTag))
+            ExitTrigger.Raise();
+    }
+}
