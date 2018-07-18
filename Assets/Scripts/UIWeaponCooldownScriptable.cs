9e739f41106771b88b763d1c23184e7d205eeadd
Ralenski Doucet <Ralenskid@gmail.com>
2018-07-18 12:54:58 -0500

    Added Weapon Cool Down Script

---
+++ Assets/Scripts/UIWeaponCooldownScriptable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Cooldown")]
+public class UIWeaponCooldownScriptable : IntVariable
+{
+
+    public int MaxTemp;
+    public int Temp;
+
+    public override int Value
+    {
+        get { return Temp; }
+    }
+
+    public override int MaxValue
+    {
+        get { return MaxTemp; }
+    }
+
+    public virtual void HeatWeapon(int amount)
+    {
+        Temp += amount;
+        if (Temp < 100) Debug.Log("Weapon Cool.");
+        if (Temp == 100) Debug.Log("Weapon OVer Heated.");
+    }
+}
