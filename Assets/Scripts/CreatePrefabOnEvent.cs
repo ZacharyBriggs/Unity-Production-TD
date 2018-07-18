7e465c307d1395cb1a1f6d45b9d2ea8549aff433
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-18 14:12:43 -0500

    utili function to create a prefab when an event happens

---
+++ Assets/Scripts/CreatePrefabOnEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePrefabOnEvent : MonoBehaviour
+{
+    public Transform startTransform;
+
+    public GameObject Prefab;
+    // Use this for initialization
+
+    public void CreatePrefab()
+    {
+        var go = Instantiate(Prefab, startTransform);
+        Destroy(go, .5f);
+    }
+}
