3457f6f295196b7eeade261d22a7675378f4a7be
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-18 14:12:09 -0500

    enemies dieing again

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,18 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class EnemyBehaviour : MonoBehaviour, IDamageable
 {
     public int Health;
-    [NonSerialized]public int MaxHealth;
+    [NonSerialized] public int MaxHealth;
     public int Damage = 10;
     private GameObject _target;
     private HealthScriptable _healthScript;
     private bool IsAttacking;
     public GameEvent OnEnemyDamageTaken;
     public GameEvent OnEnemyAttacked;
-    private GameEvent OnEnemyDied;
+    public UnityEvent OnEnemyDied;
     private Animator animator;
     private NavMeshAgent _navAgent;
     private IDamageable attackTarget;
@@ -23,19 +24,22 @@ public class EnemyBehaviour : MonoBehaviour, IDamageable
     private void Start()
     {
         animator = GetComponent<Animator>();
-        OnEnemyDied = ScriptableObject.CreateInstance<GameEvent>();
         _healthScript = ScriptableObject.CreateInstance<HealthScriptable>();
         _healthScript.Health = this.Health;
         MaxHealth = _healthScript.Health;
+        animator.SetInteger("Health", _healthScript.Health);
+         
         _target = FindObjectOfType<PayloadBehaviour>().gameObject;
         _navAgent = GetComponent<NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
     private void Update()
     {
         _navAgent.destination = _target.transform.position;
-        animator.SetFloat("Speed",_navAgent.velocity.magnitude);
+        animator.SetFloat("Speed", _navAgent.velocity.magnitude);
+        animator.SetInteger("Health", _healthScript.Health);
         this.Health = _healthScript.Health;
     }
 
@@ -45,10 +49,10 @@ public class EnemyBehaviour : MonoBehaviour, IDamageable
         OnEnemyDamageTaken.Raise();
         if (_healthScript.Health <= 0)
         {
-            OnEnemyDied.Raise();
-            animator.SetBool("IsDead",true);
+            OnEnemyDied.Invoke();
+            animator.SetBool("IsDead", true);
         }
-            
+
     }
     private void Attack(IDamageable damageable)
     {
@@ -59,7 +63,7 @@ public class EnemyBehaviour : MonoBehaviour, IDamageable
     void StopAttacking()
     {
         attackTarget.TakeDamage(Damage);
-        animator.SetBool("IsAttacking",false);
+        animator.SetBool("IsAttacking", false);
     }
 
     void DestroyEnemy()
@@ -68,7 +72,7 @@ public class EnemyBehaviour : MonoBehaviour, IDamageable
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Payload") || other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Payload") || other.gameObject.CompareTag("Player"))
             Attack(other.gameObject.GetComponent<IDamageable>());
     }
 }
