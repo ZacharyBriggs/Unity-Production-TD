6a86aca856098b39a6a077992205f567dd9ac414
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-22 23:08:55 -0500

    commit

---
+++ Assets/Scripts/WaveBarBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveBarBehaviour : MonoBehaviour
+{
+    private Image WaveFill;
+
+	void Start ()
+    {
+        WaveFill = GetComponent<Image>();
+	}
+
+	void Update ()
+    {
+
+	}
+}
